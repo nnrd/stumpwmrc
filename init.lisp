0bae0cef2279de89820cbbb7de0425f22f54d761
nonamenerd <nonamenerd@gmail.com>
2016-06-18 15:47:52 +0500

    Wallpaper var

--- init.lisp
+++ init.lisp
@@ -13,6 +13,8 @@
 ;; (set-font (make-instance 'xft:font :family "Droid Sans Mono" :subfamily "Regular" :size 10))
 (set-font "-*-terminus-medium-r-normal-*-14-*-*-*-*-*-iso10646-1")
 
+(defvar *wallpaper* "~/Картинки/обои/aaa/1777531278467323433.jpg")0
+
 (defvar *confdir* "/home/nnrd/.stumpwm.d")
 (defun load-conf-file (filename)
   (load (format nil "~A/~A" *confdir* filename)))
@@ -35,7 +37,7 @@
    (current-screen)
    "Starting swank. M-x slime-connect RET RET, then (in-package :stumpwm)."))
 
-(run-shell-command "wmsetbg ~/Картинки/обои/aaa/1777531278467323433.jpg")
+(run-shell-command (format nil "wmsetbg ~A" *wallpaper*))
 
 (ql:quickload :xembed)
 (load-module "stumptray")
